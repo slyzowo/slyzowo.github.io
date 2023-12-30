7e2e5c12d220f1b883ddba8fa4b8e7dedab9fd0f
slyzoo <gavinhimes@gmail.com>
2023-12-30 00:28:48 -0600

    fixed more things

--- cyphers/ascii_translator.js
+++ cyphers/ascii_translator.js
@@ -11,3 +11,25 @@ function translateToASCII() {
 
     outputDiv.textContent = 'ASCII Translation:\n' + asciiText;
 }
+
+function asciiDecoder(encodedText) {
+    let decodedText = "";
+    const encodedValues = encodedText.split(' ');
+
+    encodedValues.forEach(value => {
+        try {
+            const character = String.fromCharCode(parseInt(value));
+            decodedText += character;
+        } catch (error) {
+            decodedText += "[Invalid ASCII]";
+        }
+    });
+
+    return decodedText;
+}
+
+function decode() {
+    const encodedText = document.getElementById('encoded_text').value;
+    const decodedText = asciiDecoder(encodedText);
+    document.getElementById('decoded_text').textContent = "Decoded Text: " + decodedText;
+}
