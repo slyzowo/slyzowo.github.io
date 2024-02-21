549931d9004f894e542a7ae8bf2b29f207a3dda9
slyzoo <gavinhimes@gmail.com>
2024-02-21 11:16:39 -0600

    made all the buttons on calc work

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -1,16 +1,30 @@
 const display = document.getElementById("display");
 
-function appendToDisplay(input){
+function appendToDisplay(input) {
     display.value += input;
 }
-function clearDisplay(){
+
+function clearDisplay() {
     display.value = "";
 }
-function calculate(){
-    try{
-        display.value = eval(display.value);
+
+function calculate() {
+    try {
+        let expression = display.value;
+        if (expression.includes("%")) {
+            expression = expression.replace(/%/, "/ 100");
+        }
+
+        expression = expression.replace(/cos/g, "Math.cos");
+        expression = expression.replace(/tan/g, "Math.tan");
+        expression = expression.replace(/sin/g, "Math.sin");
+
+        display.value = eval(expression);
+    } catch (error) {
+        display.value = "Error";
     }
-    catch(error){
-        display.value = "Error"
-    }
+}
+
+function sqrt() {
+    display.value = Math.sqrt(parseFloat(display.value));
 }
