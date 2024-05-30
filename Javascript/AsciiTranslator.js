56bf8e58cea29cff2b0806a245bb3794c4abc261
slyzoo <gavinhimes@gmail.com>
2024-05-30 01:14:52 -0500

    added "copy to clipboard" on ascii

--- Javascript/AsciiTranslator.js
+++ Javascript/AsciiTranslator.js
@@ -13,3 +13,10 @@ function decodeFromASCII() {
     let chars = inputText.split(' ').map(code => String.fromCharCode(code));
     document.getElementById('outputText').value = chars.join('');
 }
+
+function copyToClipboard() {
+    const outputText = document.getElementById('outputText');
+    outputText.select();
+    document.execCommand('copy');
+    alert('Output has been copied to your clipboard');
+}
