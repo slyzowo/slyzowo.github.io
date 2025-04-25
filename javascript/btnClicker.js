50d483723f06a61773eec9c6dfe69cdaa1037c32
slyzoo <lilithhimes@gmail.com>
2025-04-25 14:16:41 -0500

    Fixed Navigation Links
    Added :
        None
    Changed :
        Fixed Navigation with proper links
    Deleted :
        None

--- javascript/btnClicker.js
+++ javascript/btnClicker.js
@@ -1,7 +1,7 @@
 const counterDisplay = document.getElementById("counter-display");
 
 const decreaseButton = document.getElementById("decrease-btn");
-const resetButton = document.getElementById("reset-btn");
+const resetButton = document.getElementById("button-clicker-reset-btn");
 const increaseButton = document.getElementById("increase-btn");
 
 let count = 0;
